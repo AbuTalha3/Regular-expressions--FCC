3b368094eda12fba5c02de0f346c829ca9329cff
AbuTalha3 <najeebakhun@gmail.com>
2024-04-16 01:28:04 +0500

    my final commit

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ const checkMessageButton = document.getElementById("check-message-btn");
 const helpRegex = /please help|assist me/i;
 const dollarRegex = /[0-9]+ (?:hundred|thousand|million|billion)? dollars/i;
 const freeRegex = /(?:^|\s)fr[e3][e3] m[o0]n[e3]y(?:$|\s)/i;
+const stockRegex = /(?:^|\s)[s5][t7][o0][c{[(]k [a@4]l[e3]r[t7](?:$|\s)/i;
+const dearRegex = /d[e3][a@4]r fr[i1|][e3]nd/i;
 
-const denyList = [helpRegex, dollarRegex, freeRegex];
+const denyList = [helpRegex, dollarRegex, freeRegex, stockRegex, dearRegex];
 
 const isSpam = (msg) => denyList.some((regex) => regex.test(msg));
 
