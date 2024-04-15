cedd0c77793e4ee56e71363be0e643d809b2c9c6
AbuTalha3 <najeebakhun@gmail.com>
2024-04-16 00:27:25 +0500

    Add step 25

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const checkMessageButton = document.getElementById("check-message-btn");
 
 const helpRegex = /please help|assist me/i;
 const dollarRegex = /[0-9]+ (?:hundred|thousand|million|billion)? dollars/i;
-const freeRegex = /fr[e3][e3] mon[e3]y/i;
+const freeRegex = /\sfr[e3][e3] m[o0]n[e3]y\s/i;
 
 const denyList = [helpRegex, dollarRegex, freeRegex];
 
